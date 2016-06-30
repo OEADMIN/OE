7eadc016f662ff7de30dacb3d45c718a0b5a9025
xjouyi <xjouyi@163.com>
2016-06-30 21:05:54 +0800

    add javadoc set

--- apiservice/src/main/java/com/openexpense/controller/HostController.java
+++ apiservice/src/main/java/com/openexpense/controller/HostController.java
@@ -6,19 +6,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by jinyi on 16/6/13.
+/**用户登录接口
+ *2016/06/30.
+ *@author xjouyi@163.com
+ *@version 0.1
  */
 @Controller
 @RequestMapping("/host")
 public class HostController {
 
+    /**用户登录
+    *@param id type:string 用户id
+    *@param pass type:string 密码
+    *@return id + pass
+    */
     @ResponseBody
     @RequestMapping(value = "/signin/{id}/{pass}",method = RequestMethod.GET)
     public String signIn(@PathVariable("id")String id,@PathVariable("pass")String pass){
         return id+","+pass;
     }
 
+    /**用户注销
+    *@return "hello"
+    */
     @ResponseBody
     @RequestMapping(value = "/signout",method = RequestMethod.GET)
     public String signOut(){
