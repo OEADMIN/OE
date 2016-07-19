666e4f4360bf291035ae19f1817d1b1801df780d
Jayie <jayie@duomai.com>
2016-07-19 18:11:23 +0800

    update login

--- front/web/http.js
+++ front/web/http.js
@@ -1,47 +1,38 @@
 var request = require('request');
-var host_connection = "http://api.jayie.cn";
+var host_connection = "https://api.jayie.cn";
 
 function async (type,url,params,callback){
 	var option = {
 		url:host_connection+url,
 		method:type,
 		formData:params,
+		oauth:params,
+		json:true,
 		callback :function(error,response,body){
-			callback(body);
+			var obj = {success:"fail",code:"",data:[{message:"404 not found"}]}
+			if(response.statusCode==404){
+				callback(obj);
+			}else if(response.statusCode>500){
+				callback(body);
+			}else if(response.statusCode>=200 || response.statusCode<300){
+				callback(body);
+			}else{
+				callback(obj);
+			}
 		}
 	}
 	request(option);
 }
 module.exports.get = function(url,params,callback){
 	async("GET",url,params,callback);
-	// request.get({
-	// 	url:host_connection+url,
-	// },function(error, response, body){
-	// 	callback(body);
-	// });
 }
 
 module.exports.post = function(url,params,callback){
 	async("POST",url,params,callback);
-	// request.post({
-	// 	url:host_connection+url,
-	// },function(error, response, body){
-	// 	callback(response);
-	// });
 }
 module.exports.put = function(url,params,callback){
 	async("PUT",url,params,callback);
-	// request.put({
-	// 	url:host_connection+url,
-	// },function(error, response, body){
-	// 	callback(response);
-	// });
 }
 module.exports.delete = function(url,params,callback){
 	async("DELETE",url,params,callback);
-	// request.delete({
-	// 	url:host_connection+this.url,
-	// },function(error, response, body){
-	// 	callback(response);
-	// });
 }
